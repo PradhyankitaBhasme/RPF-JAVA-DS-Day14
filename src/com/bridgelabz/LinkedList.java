60561eeb9c63cc73022880229b37576b3a0e5018
sagar <chavansagar96044@gmail.com>
2022-07-26 20:41:05 +0530

    delete 1st element using pop method

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,8 +1,8 @@
 package com.bridgelabz;
 
 public class LinkedList<K> {
-    private Node head;
-    private Node tail;
+    private Node<K> head;
+    private Node<K> tail;
 
     public void add(K key){
         Node<K> node=new Node<>(key);
@@ -29,11 +29,17 @@ public class LinkedList<K> {
         node.setNext(tail);
         head.setNext(node);
     }
+    public K pop(){
+        K deleteData = head.getData();
+        this.head=head.getNext();
+        return deleteData;
+    }
     public static void main(String[] args) {
         LinkedList linkedList= new LinkedList();
+        linkedList.append(56);
         linkedList.append(30);
         linkedList.append(70);
 
-        linkedList.insert(56);
+        System.out.println(linkedList.pop());
     }
 }
